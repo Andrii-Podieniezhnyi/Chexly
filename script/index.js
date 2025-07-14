9a8292e0e3464f9709a0e465a0de67a892619b5e
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-07-15 02:27:34 +0300

    add and delete task item

--- script/index.js
+++ script/index.js
@@ -10,3 +10,47 @@ createdTabsBtns.forEach(function (btn) {
         btn.classList.add('active');
     })
 })
+
+
+
+// add and delete task item
+
+const newTaskInput = document.getElementById('new-task-input');
+const addNewTaskBtn = document.querySelector('.add-new-task-btn');
+const taskList = document.querySelector('.task-list');
+
+
+
+addNewTaskBtn.addEventListener('click', () => {
+    newTaskText =  newTaskInput.value.trim();
+
+    if (newTaskText == ''){
+        alert('Введіть завдання!');
+
+        return;
+    }
+
+    const taskId = 'task-' + Date.now();
+
+    const newLi = document.createElement('li');
+    newLi.className = 'task-item';
+
+
+    newLi.innerHTML = `
+        <input type="checkbox" id = "${taskId}" class="task-checkbox">
+        <label for="${taskId}" class="task-label">"${newTaskText}"</label>
+        <button class="task-delete">🗑️</button>
+    `
+
+    taskList.appendChild(newLi);
+
+    newTaskInput.value = '';
+
+
+    const delBtn = newLi.querySelector('.task-delete');
+
+        delBtn.addEventListener('click', () => {
+            newLi.remove();
+        })
+
+})
