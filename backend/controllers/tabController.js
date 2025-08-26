0ec88e9a9d41cdf8646bd4dfee9808a1a7bc8f22
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-08-26 21:01:29 +0300

    added deleteTab function to tabController

--- backend/controllers/tabController.js
+++ backend/controllers/tabController.js
@@ -1,4 +1,7 @@
 import Tab from "../models/Tab";
+import Task from "../models/Task.js";
+import mongoose from "mongoose";
+
 
 
 // створення вкладки
@@ -45,4 +48,37 @@ export const getTabs = async (req, res) => {
         res.status(500).json({ message: 'Помилка при отриманні вкладок', error: message.error})
     }
     
+}
+
+
+
+// видалення вкладки
+
+
+
+export const deleteTab = async(req, res) => {
+    try {
+        
+        const {id} = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({message: 'Некоректний ID вкладки'});
+        }
+
+        const tab = await Tab.finndOne({_id: id, userId: req.user.id});
+
+        if (!tab) {
+            return res.status(404).json({message: 'Вкладка не знайдена або не належить користувачу'});
+        }
+
+        await Task.deleteMany({tabId: id});
+        await tab.deleteOne();
+
+        res.status(200).json({message: 'Вкладка та всі її завдання успішно видалені'});
+
+
+    } catch (error) {
+        console.error('Помилка при видаленні вкладки:', error);
+        res.status(500).json({message: 'Помилка при видаленні вкладки', error: error.message});
+    }
 }
