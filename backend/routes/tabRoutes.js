63592f0e05b0c6fd541d1c705bbface0b9cc77c0
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-09-22 22:14:51 +0300

    + updateTab route

--- backend/routes/tabRoutes.js
+++ backend/routes/tabRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createTab, getTabs, deleteTab } from '../controllers/tabController.js';
+import { createTab, getTabs, deleteTab, updateTab } from '../controllers/tabController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/tabs', authMiddleware, createTab);
 router.get('/tabs', authMiddleware, getTabs);
 router.delete('/tabs/:id', authMiddleware, deleteTab);
+router.put('/tabs/:id', authMiddleware, updateTab)
 
 export default router;
